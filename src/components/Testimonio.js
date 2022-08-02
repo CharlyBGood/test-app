a961a2c1f53b61abf08bf09e306f8aeaccd89301
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-08-02 14:23:19 -0300

    finish clone

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,29 +1,25 @@
 import React from "react";
 import "../stylesheets/Testimonio.css";
 
-
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="container-test">
       <img
         className="img-test"
-        src={require("../img/test-emma.png")}
+        src={require(`../img/test-${props.img}.png`)}
         alt="Foto de Emma"
       />
       <div className="container-txt-test">
-        <p className="name-test">Emma Bostian in Sweden</p>
-        <p className="job-test">Software Engineer at Spotify</p>
-        <p className="txt-test">
-          "I've always struggled with learning JavaScript. I've taken many
-          courses but freeCodeCamp's course was the one which stuck. Studying
-          JavaScript as well as data structures and algorithms on freeCodeCamp
-          gave me the skills and confidence I needed to land my dream job as a
-          software engineer at Spotify."
+        <p className="name-test">
+          <strong>{props.name}</strong> in {props.country}
         </p>
+        <p className="job-test">
+          {props.job} at <strong>{props.company}</strong>
+        </p>
+        <p className="txt-test">"{props.testimony}"</p>
       </div>
     </div>
   );
 }
-
 
 export default Testimonio;
